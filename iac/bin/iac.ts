90b6399b441a3ba75085bea30afe302125ac600f
dev-corelift <devteam@corelift.io>
2025-06-15 09:13:55 -0500

    chore: remove deprecated IAC directory and associated AWS CDK configuration

--- iac/bin/iac.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { SimpleApiStack } from '../lib/simple-api-stack';
-
-const app = new cdk.App();
-new SimpleApiStack(app, 'SimpleApiStack', {
-  env: { account: '000000000000', region: 'us-east-1' },
-});
